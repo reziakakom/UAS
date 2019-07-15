3907d64394856f1c5d1b87a65fac5e36846a4492
reziakakom <52922970+reziakakom@users.noreply.github.com>
2019-07-15 20:52:48 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php 
+$host       = 'localhost';
+$user       = 'root';
+$password   = '';
+$db         = 'mahasiswa';
+
+
+$konek = mysqli_connect($host, $user, $password, $db) or die(mysqli_error());
+?>
